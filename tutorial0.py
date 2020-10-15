8faf39b2c3b3d76a348dccb21474996d3ab2d931
RockingAli <alihasank86@gmail.com>
2020-10-15 14:36:58 +0530

    Tutorial for getting event data for a single match

--- tutorial0.py
+++ tutorial0.py
@@ -6,44 +6,57 @@ Created on Wed Oct 14 14:28:34 2020
 @twitter: rockingAli5 
 """
 
+"""
+A tutorial on getting Event Data for a single match, 
+with an example of Passing Network for a team.
+"""
+
+
 
 import pandas as pd
 import numpy as np
-from mplsoccer.pitch import Pitch
-from matplotlib.colors import to_rgba
 from selenium import webdriver
-
-from main import (getLeagueLinks, getMatchData, getMatchLinks, getTeamData, getTeamLinks,
-                  createMatchesDF, createEventsDF)
-
+from main import (getMatchData,  createMatchesDF, createEventsDF)
 from visuals import createPassNetworks
 
 
 
 
-###     Get Match Data     ###
+# Get Match Data  (Run from line 29 to line 35 together)   
 if __name__ == "__main__":
     driver = webdriver.Chrome('chromedriver.exe')
-# whoscored url to the required match
+    
+    
+# whoscored match centre url of the required match (Example: Barcelona vs Sevilla)
 url = "https://www.whoscored.com/Matches/1491995/Live/Spain-LaLiga-2020-2021-Barcelona-Sevilla"
 match_data = getMatchData(driver, url)
 
 
+# Match dataframe containing info about the match
 matches_df = createMatchesDF(match_data)
+
+
+# Events dataframe      
 events_df = createEventsDF(match_data)
 
 
+# match Id
+matchId = match_data['matchId']
 
 
-###     Create Pass Network     ###
+# Information about respective taems as dictionary
+home_data = matches_df['home'][matchId]
+away_data = matches_df['away'][matchId]
 
+
+
+# Pass Network Example from Barcelona vs Sevilla game     
 team = 'Barcelona'
 teamId = 65
 opponent = 'Sevilla'
-matchId = match_data['matchId']
 venue = 'home'
 
-
+# Create Pass Network     
 createPassNetworks(matches_df, events_df, matchId, teamId, team, opponent, venue)
 
 
